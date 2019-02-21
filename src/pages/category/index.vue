3740bb9fd79a9678425c70b401f3b6b4398c6dba
wanqihuan <854238120@qq.com>
2019-02-21 15:48:57 +0800

    分类数据渲染

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -2,22 +2,28 @@
   <div class="category">
     <!-- 搜素 -->
   <search></search>
+  <!-- 主要内容 -->
     <div class="main">
+      <!-- 左边菜单栏 -->
       <div class="main_left">
         <ul v-for="(item,index) in categoriesData" :key="index">
-          <li >{{item.cat_name}}</li>
-          <!-- <li class="item">热门推荐</li> -->
+          <li @click="handertoggle(index)" :class="{active:num==index}">{{item.cat_name}}</li>
         </ul>
       </div>
+      <!-- 右边产品 -->
       <div class="main_right">
         <div class="main_right_theme">
           <img src="http://www.huangjiangjun.top/pyg/banner2.png" alt="">
         </div>
-        <div class="main_right_content" >
-           <div class="main_right_title" v-for="(item,index) in categoriesinit" :key="index">/　{{item.cat_name}}　/ </div>
-
+        <div class="main_right_content" v-for="(item,index) in categoriesList" :key="index" >
+           <div class="main_right_title" >/　{{item.cat_name}}　/ </div>
+           <ul class="main_right_list">
+             <li v-for="(item1,index1) in item.children" :key="index1">
+                 <img :src="item1.cat_icon" alt="">
+                 <p>{{item1.cat_name}}</p>
+             </li>
+           </ul>
         </div>
-
       </div>
     </div>
   </div>
@@ -31,8 +37,9 @@ import request from "@/utils/request";
 export default {
   data () {
     return {
+      num:0,
       categoriesData:[],
-      categoriesinit:[]
+      categoriesList:[]
     }
   },
 
@@ -45,13 +52,18 @@ export default {
     .then(res=>{
       console.log(res);
       this.categoriesData=res.data.message
-      // console.log(this.categoriesData[0]);
-      this.categoriesinit=res.data.message[0].children
-      console.log(this.categoriesinit);
+      this.categoriesList=res.data.message[0].children
+      // console.log(this.categoriesinit);
     })
    },
   methods: {
-
+    // 根据索引切换数据
+    handertoggle(index){
+      // console.log(index);
+      this.categoriesList=this.categoriesData[index].children
+      // 切换left菜单栏样式
+      this.num=index
+    }
   },
 
   created () {
@@ -63,11 +75,16 @@ export default {
 <style lang="scss" scoped>
  .category{
   .main{
+    // overflow: scroll;
     display: flex;
-    height: 100%;
-    // overflow: hidden;
+    // height: 100%;
+    // position: fixed;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    top: 98rpx;
     .main_left{
-       width: 198rpx;
+       width: 200rpx;
        background-color: #f4f4f4;
        ul{
          >li{
@@ -84,6 +101,7 @@ export default {
        }
     }
     .main_right{
+      flex: 1;
       padding: 20rpx;
      height: 100%;
      width: 100%;
@@ -97,6 +115,21 @@ export default {
       .main_right_title{
         padding: 20rpx 0;
         text-align: center;
+      }
+      .main_right_list{
+        display: flex;
+        flex-wrap: wrap;
+        li{
+          width: 33.33%;
+          text-align: center;
+          img{
+            width: 120rpx;
+            height: 120rpx;
+          }
+          p{
+            font-size: 30rpx;
+          }
+        }
       }
 
 
