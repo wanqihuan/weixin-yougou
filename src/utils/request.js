2833421dc55bc533ff62b347354c5bfa87f98c77
wanqihuan <854238120@qq.com>
2019-02-22 17:20:23 +0800

    完善封装的request

--- src/utils/request.js
+++ src/utils/request.js
@@ -12,5 +12,9 @@ function request(url, method = "GET", data = {}) {
     })
   })
 }
+request.baseURL = "https://www.zhengzhicheng.cn/api/public/v1/";
+request.get = function (url, data) {
+  return request(request.baseURL + url, "GET", data);
+};
 // 导出request
 export default request
